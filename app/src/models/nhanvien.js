d9ba39e2b2c12fbf15da270100bd619bb3122cdd
Duong Thanh Tam <thanhtamduong555@gmail.com>
2023-02-19 21:05:15 +0700

    JS

---
+++ app/src/models/nhanvien.js
@@ -0,0 +1,15 @@
+function NhanVien(
+  _maNhanVien,
+  _tenNhanVien,
+  _chucVu,
+  _heSoChucVu,
+  _luongCoBan,
+  _soGioLamTrongThang
+) {
+  this.maNhanVien = _maNhanVien;
+  this.tenNhanVien = _tenNhanVien;
+  this.chucVu = _chucVu;
+  this.heSoChucVu = _heSoChucVu;
+  this.luongCoBan = _luongCoBan;
+  this.soGioLamTrongThang = _soGioLamTrongThang;
+}
